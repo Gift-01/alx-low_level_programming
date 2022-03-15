aed424ddc585c3104ca86ae4dc43e18c2c3a01fa
Gift-01 <kristagift@gmail.com>
2022-03-15 01:33:41 -0700

    my commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ else if (n % == 0)
 
 else
 {
-printf("Last digit of %i is %i amd is less than 6 and not 0\n", n. n % 10);
+printf("Last digit of %i is %i amd is less than 6 and not 0\n", n, n % 10);
 }
 
 return(0);
