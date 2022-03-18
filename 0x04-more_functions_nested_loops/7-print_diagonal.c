ed75bde2dfb50a58a46fccb8d3024d8aa9096dc1
Gift-01 <kristagift@gmail.com>
2022-03-19 00:03:41 +0100

    7-pd

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal lines
+ * @n: number of spaces before the line
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+for (int a = 0 ; a < n; a++)
+{
+for (int b = 0 ; b < a; b++)
+{
+_putchar(' ');
+}
+_putchar('\\\n');
+}
+}
