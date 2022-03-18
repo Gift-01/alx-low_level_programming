506c5e143a948a860ebd90b8a46f2775be51c4b2
Gift-01 <kristagift@gmail.com>
2022-03-19 00:36:16 +0100

    8-ps

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print something
+ * @size: some sizw
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int a, b;
+
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
