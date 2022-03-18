880aa4eb254c9568a69513a0767f2e55bd34361d
Gift-01 <kristagift@gmail.com>
2022-03-18 22:38:35 +0100

    add 6-pl

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_lines - Prints line based on numbers recieved
+ *
+ * @n : integer
+ * Return: void.
+ */
+
+void print_lines(int n)
+{
+int w;
+
+for (w = 0; w < n; w++)
+{
+_putchar('_');
+}
+_putchar('\n');
+
+}
