180c5b4a43c634796ebe303fa3f5b752216aba3d
Gift-01 <kristagift@gmail.com>
2022-03-19 00:58:15 +0100

    add 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a % 5 == 0 && a % 3 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", a);
+}
+
+}
+printf("\n");
+return 0;
+}
