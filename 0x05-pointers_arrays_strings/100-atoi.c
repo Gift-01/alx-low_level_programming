456a92b76e07f4a19fbc45061690a270e3a7fc24
Gift-01 <kristagift@gmail.com>
2022-03-23 03:58:10 +0100

    My commit message

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ return (n >= '0' && n <= '9');
 
 /**
 * _atoi - convert a string to an integer
-*@s; string
+*@s: string
 *Return: Return the num
 */
 int _atoi(char *s)
@@ -27,7 +27,7 @@ for (i = 0; s[i] != '\0'; i++)
 {
 if (is_numerical(s[i]))
 {
-number = (s[i] - 48) + number * 10;
+number = (s[i] - 48) + number *10;
 
 if (s[i + 1] == ' ')
 break;
