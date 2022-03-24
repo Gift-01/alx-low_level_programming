473761fcb0af76b0a6a8bbf91302cb7dfdca9804
Gift-01 <kristagift@gmail.com>
2022-03-24 13:45:02 +0100

    My zero task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,15 +7,14 @@
 *Return: dest
 */
 
-char *_strncat(char *dest, char *src, int n);
-
+char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
 
 i = 0;
 while (dest[i] != '\0')
 {
-i++
+i++;
 }
 
 j = 0;
