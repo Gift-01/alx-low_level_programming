cdff1a10386214d87a136b49fdf95e29eae94690
Gift-01 <kristagift@gmail.com>
2022-03-25 06:06:19 +0100

    add magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -18,7 +18,7 @@ int *p;
    */
   ;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-printf("%c[2] = %d\n", 'a', 98); 
+*(p + 5) = 98;
+  printf("a[2] = %d\n", a[2]); 
   return (0);
 }
